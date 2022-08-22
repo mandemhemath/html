05aa4f099f7f162e7cba12ebb1177bbd38565f13
hemanth <hemanthmandem95@gmail.com>
2022-08-22 22:16:26 +0530

    out put 98

---
+++ Test.java
@@ -0,0 +1,8 @@
+class Test{
+    public static void main(String[] args){
+
+        byte b=100;
+        System.out.println(b);
+    }
+
+}
---
+++ basics/HTML/hemanth.java
@@ -0,0 +1,6 @@
+class hemanth{
+    public static void main(String args[])
+    {
+        System.out.println("hello hemanth");
+    }
+}
